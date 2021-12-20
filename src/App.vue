ee19cc8876f1e902cdfba65fbb74c9decc6d6e71
NoDream <1802024110@qq.com>
2021-12-20 11:25:49 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <div  style="width:300px;margin:50% auto">
+    <v-text-field
+    class="rounded-pill"
+          solo
+          hide-details='auto'
+          label="请输入分享链接"
+          v-model="text"
+    ></v-text-field>
+    <!-- 链接显示 -->
+    <v-textarea
+    class="pt-4"
+          outlined
+          readonly
+          name="input-7-4"
+          label="下载链接"
+          :value='downloadUrl'
+        ></v-textarea>
+  <!-- 下载视频 -->
+    <v-btn
+      class="rounded-pill"
+      color="primary"
+      @click="download"
+      >下载</v-btn>
+    </div>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      text: "#轻视频# https://bbq.bilibili.com/video/?id=1635755626020555348&shu=gn871",
+    }
+  },
+  computed: {
+    downloadUrl() {
+      // 正则表达式
+      let reg = /https:\/\/bbq.bilibili.com\/video\/\?id=(\d+)&shu=gn871/;
+      let url = this.text;
+      let id = url.match(reg)[1];
+      // axiso访问
+      axios.get(`https://bbq.bilibili.com/bbq/app-bbq/sv/detail?svid=${id}&version=1.3.0&platform=h5`).then(res => {
+        console.log(res.data);
+      })
+      return id;
+    }
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
